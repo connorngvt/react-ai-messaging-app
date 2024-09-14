c67e1c75e8fe7176071b523976127d86b5491751
James Kang <jameskang@Jamess-MacBook-Pro.local>
2024-09-14 11:49:49 -0400

    login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react"
-
+import LoginForm from "./LoginForm"
+import './LoginForm.css'
+import { FaUser } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
 function App() {
 
   return (
     <>
-      
+      <LoginForm />
     </>
   )
 }

4857f87f145e18a1d019dc57f558fb888324f107
James Kang <jameskang@Jamess-MacBook-Pro.local>
2024-09-14 17:19:06 -0400

    added index title

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,7 @@
 import React from "react"
 import LoginForm from "./LoginForm"
 import './LoginForm.css'
-import { FaUser } from "react-icons/fa";
-import { FaLock } from "react-icons/fa";
+
 function App() {
 
   return (

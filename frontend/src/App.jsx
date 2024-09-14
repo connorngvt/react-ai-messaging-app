3e143a909b5663a210d574d1e8848faac106a64f
James Kang <jameskang@Jamess-MacBook-Pro.local>
2024-09-14 09:40:01 -0400

    no

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react"
 
 function App() {
-  return (
-    <div>
 
-    </div>
-  );
+  return (
+    <>
+      
+    </>
+  )
 }
 
-export default App;
+export default App
